0b676a479c5ce94499c1e06094c844c79d7c4903
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2025-02-26 11:58:34 +0500

    loading with certification validation

--- 04loading/src/App.jsx
+++ 04loading/src/App.jsx
@@ -2,18 +2,14 @@ import { use, useState } from "react"
 
 function App() {
   const [loading, setLoading] = useState(true)
-  const [certifications, setCertifications] = useState(["HTML", "CSS", "JavaScript"])
+  const [certifications, setCertifications] = useState([])
   setTimeout(() => {
     setLoading(false)
   }
   , 3000)
   return (
     <>
-    {loading ? <p>Loading...</p>:
-    certifications.length>0 && certifications.map((certification, index) => (
-      <p key={index}>{certification}</p>
-    ))  
-    }
+   {loading ? <h1>Loading...</h1> : certifications.length > 0 ? <ul>{certifications.map((certification, index) => <li key={index}>{certification}</li>)}</ul> : <h1>No certifications found</h1>}
     </>
 
   )
