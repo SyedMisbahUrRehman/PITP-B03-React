69e19c71cee83de18c2f9ecd8acb790810c77e2d
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2025-02-26 11:42:13 +0500

    accordian

--- 03mapapp/src/Accordian.jsx
+++ 03mapapp/src/Accordian.jsx
@@ -1,11 +1,12 @@
 import Accordion from 'react-bootstrap/Accordion';
 
-function AllCollapseExample() {
+function MyAccordian() {
   return (
-    <Accordion>
+    <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>Accordion Item #1</Accordion.Header>
         <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
@@ -30,4 +31,4 @@ function AllCollapseExample() {
   );
 }
 
-export default AllCollapseExample;
+export default MyAccordian;
